eedbd9b309421937fee79fb960243fb0f7f105c2
Vishvajeet <Vishvajeet795@gmail.com>
2023-09-05 20:41:33 +0530

    project name changed to Student Performance Report

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='Student Performnace Analysis',
 version='0.0.1',
 author='vishvajeet',
 author_email='vishvajeet795@gmail.com',
